5c15ffda7f4a99daff687a3835f9c6734ee612ac
ibrahnerd7 <ibrahmaina7@gmail.co>
2019-10-11 19:19:17 +0300

    Change package name

---
+++ app/src/main/java/com/google/codelabs/appauth/activities/AddressActivity.java
@@ -0,0 +1,13 @@
+package com.google.codelabs.appauth.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_address);
+    }
+}
---
+++ app/src/main/java/com/google/codelabs/appauth/activities/ExternalActivity.java
@@ -0,0 +1,14 @@
+package com.google.codelabs.appauth.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ExternalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_external);
+    }
+}
